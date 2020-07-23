061c6f81647fe98d51e9f360aa01203630151929
royeradames <royeraadames@gmail.com>
2020-07-23 01:55:37 -0400

    added attributes to the inputs in forms.js

--- project/src/components/Forms.js
+++ project/src/components/Forms.js
@@ -3,48 +3,58 @@ import React from 'react';
 
 export default function Forms({values, inputChange, checkboxChange, submit, disabled, errors}){
     //event handlers
+    const onSubmit = e => {
+        e.preventDefault()
+        submit()
+    }
+    const onCheckboxChange = e => {
 
+    }
+
+    const onInputChange = e => {
+
+    }
     return(
         <div>
             {/* Name */}
             <label>Name&nbsp;
                 <input 
-                    value={}
-                    onChange={}
-                    name={}
-                    type={}
+                    value={values.name}
+                    onChange={onInputChange}
+                    name='name'
+                    type='text'
                 />
             </label>
 
             {/* Email */}
             <label>Email &nbsp;
                 <input 
-                    value={}
-                    onChange={}
-                    name={}
-                    type={}
+                    value={values.email}
+                    onChange={onInputChange}
+                    name='email'
+                    type='text'
                 />
             </label>
             {/* Password */}
             <label>Password &nbsp;
                 <input 
-                    value={}
-                    onChange={}
-                    name={}
-                    type={}
+                    value={values.password}
+                    onChange={onInputChange}
+                    name='password'
+                    type='text'
                 />
             </label>
             {/* Terms of Service (checkbox) */}
             <label>Terms of Service&nbsp;
                 <input 
-                    onChange={}
-                    name={}
-                    type={}
-                    checked={}
+                    onChange={onCheckboxChange}
+                    name='Terms of Service'
+                    type='checkbox'
+                    checked={values.termsOfService === true}
                 />
             </label>
             {/* A Submit button to send our form data to the server */}
-
+            <button disable={disabled}>Submit</button>
         </div>
     )
 }
