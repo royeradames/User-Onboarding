12268dab94f2542169ffb61a3a42cf105b0f2280
royeradames <royeraadames@gmail.com>
2020-07-23 01:02:46 -0400

    added skeleton of require fields

--- project/src/components/Forms.js
+++ project/src/components/Forms.js
@@ -1,10 +1,49 @@
 import React from 'react';
 
 
-export default function Forms({}){
+export default function Forms({values, inputChange, checkboxChange, submit, disabled, errors}){
+    //event handlers
 
     return(
         <div>
+            {/* Name */}
+            <label>Name&nbsp;
+                <input 
+                    value={}
+                    onChange={}
+                    name={}
+                    type={}
+                />
+            </label>
+
+            {/* Email */}
+            <label>Email &nbsp;
+                <input 
+                    value={}
+                    onChange={}
+                    name={}
+                    type={}
+                />
+            </label>
+            {/* Password */}
+            <label>Password &nbsp;
+                <input 
+                    value={}
+                    onChange={}
+                    name={}
+                    type={}
+                />
+            </label>
+            {/* Terms of Service (checkbox) */}
+            <label>Terms of Service&nbsp;
+                <input 
+                    onChange={}
+                    name={}
+                    type={}
+                    checked={}
+                />
+            </label>
+            {/* A Submit button to send our form data to the server */}
 
         </div>
     )
